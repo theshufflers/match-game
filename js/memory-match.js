4ef2136cdc39c51249026b1ae6891e7411f366f4
Michael A. Chapman <chapmanm305@gmail.com>
2019-04-09 16:06:36 -0700

    Several major changes:
    Add functionality to render function
    
    Add functionality for card clicking and adding to compare array
    
    Add functionality for selecting another image to run compare function
    
    Add functionality for unstaging card when clicked a second time
    
    Add basic functionality in compare function

--- js/memory-match.js
+++ js/memory-match.js
@@ -5,15 +5,15 @@
 // -------------------------------------------------------
 
 var DATA = [
-  ['a', 'https://via.placeholder.com/200x250', '1', false, false],
-  ['b', 'https://via.placeholder.com/200x250', '1', false, false],
-  ['c', 'https://via.placeholder.com/200x250', '2', false, false],
-  ['d', 'https://via.placeholder.com/200x250', '2', false, false],
-  ['e', 'https://via.placeholder.com/200x250', '3', false, false],
-  ['f', 'https://via.placeholder.com/200x250', '3', false, false],
-  ['g', 'https://via.placeholder.com/200x250', '4', false, false],
-  ['h', 'https://via.placeholder.com/200x250', '4', false, false],
-  ['i', 'https://via.placeholder.com/200x250', '5', false, false],
+  ['a', 'https://via.placeholder.com/200x250', ['1', '1'], false, false],
+  ['b', 'https://via.placeholder.com/200x250', ['1', '2'], false, false],
+  ['c', 'https://via.placeholder.com/200x250', ['2', '1'], false, false],
+  ['d', 'https://via.placeholder.com/200x250', ['2', '2'], false, false],
+  ['e', 'https://via.placeholder.com/200x250', ['3', '1'], false, false],
+  ['f', 'https://via.placeholder.com/200x250', ['3', '2'], false, false],
+  ['g', 'https://via.placeholder.com/200x250', ['4', '1'], false, false],
+  ['h', 'https://via.placeholder.com/200x250', ['4', '2'], false, false],
+  ['i', 'https://via.placeholder.com/200x250', ['5', '1'], false, false],
 ];
 
 // ------------------------------------------------------
@@ -25,44 +25,71 @@ var matches = 0;
 var peeks = 0;
 
 var COMPARE_ARR = [];
-var CARDS_ARR = [];
+var CARDS_OBJ = {};
 
 // ------------------------------------------------------
 //  Defined Functions
 // -------------------------------------------------------
 
-
-function CARDS(id, imageURL, pairID, inPlay, showing) {
+function CARDS(id, imageURL, pairId, inPlay, showing) {
   this.id = id;
   this.img = imageURL;
-  this.pairID = pairID;
+  this.pairId = pairId;
   this.inPlay = inPlay;
   this.showing = showing;
-  
-  CARDS_ARR.push(this);
+
+  CARDS_OBJ[id] = this;
   this.render();
 }
 
-CARDS.prototype.toggleSelect = function() {
-  // toggle function
+CARDS.prototype.toggleSelect = function(event) {
+  event.preventDefault();
+
+  var target = event.target;
+
+  if (COMPARE_ARR < 2) {
+    if (COMPARE_ARR[0] && COMPARE_ARR[0][0] === CARDS_OBJ[target.id].pairId[0] && COMPARE_ARR[0][1] === CARDS_OBJ[target.id].pairId[1]) {
+      COMPARE_ARR.shift();
+      CARDS_OBJ[target.id].inPlay = false;
+      CARDS_OBJ[target.id].showing = false;
+    } else {
+      COMPARE_ARR.push(CARDS_OBJ[target.id].pairId);
+      CARDS_OBJ[target.id].inPlay = true;
+      CARDS_OBJ[target.id].showing = true;
+    }
+  } else {
+    COMPARE_ARR.push(CARDS_OBJ[target.id].pairId);
+    CARDS_OBJ[target.id].inPlay = true;
+    CARDS_OBJ[target.id].showing = true;
+    Compare();
+  }
 };
 
 CARDS.prototype.render = function() {
-  let cardsSection = document.getElementById('cards-section');
+  var cardsSection = document.getElementById('cards-section');
 
-  let div = document.createElement('div');
-  div.setAttribute('id', this.id);
+  var div = document.createElement('div');
   cardsSection.appendChild(div);
 
-  let img = document.createElement('img');
+  var img = document.createElement('img');
   img.setAttribute('src', 'https://via.placeholder.com/200x250');
+  img.setAttribute('id', this.id);
   div.appendChild(img);
-};
 
-console.log(CARDS_ARR);
+  img.addEventListener('click', this.toggleSelect);
+};
 
 function Compare() {
   // Compare function
+
+  if (COMPARE_ARR[0][0] === COMPARE_ARR[1][0]) {
+    console.log('RIGHT!');
+  } else {
+    console.log('WRONG');
+  }
+
+  COMPARE_ARR.shift();
+  COMPARE_ARR.shift();
 }
 
 function CorrectAnswer() {
@@ -77,7 +104,6 @@ function Shuffle() {
   // Shuffle Function
 }
 
-//Card click Event Listenter - Click
 //Reset game Event listener - Click
 
 //Create Cards function
@@ -93,7 +119,6 @@ function createCards () {
 
 function startGame() {
   createCards();
-
 }
 
 // ------------------------------------------------------
